63752bf36da5c0807140e615bdd2d6cdfaad8e3f
Anton Menshutkin <anton.mianshutkin@itechart-group.com>
2018-07-11 20:49:17 +0300

    Remove old files

--- devices/fan.js
+++
@@ -1,8 +0,0 @@
-import MainDevice from "./mainDevice";
-
-class Fan extends MainDevice {
-    constructor (room, room, power, weight, speed) {
-        super(room, room, power, weight);
-        this.speed = speed;
-    }
-};
--- devices/washing_machine.js
+++
@@ -1,8 +0,0 @@
-import MainDevice from "./mainDevice";
-
-class WashingMachine extends MainDevice {
-    constructor (room, room, power, weight, mode) {
-        super(room, room, power, weight);
-        this.mode = [];
-    }
-}
